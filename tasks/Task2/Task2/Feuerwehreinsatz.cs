7934d2525bb482aa16dd8a3cb68ef081a609d8c1
alexanderweigl <alexander.weigl@technikum-wien.at>
2017-02-21 20:06:24 +0100

    Task2 added

---
+++ tasks/Task2/Task2/Feuerwehreinsatz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Feuerwehreinsatz
+    {
+        public string ort;
+        public int alarmstufe;
+        private string beteiligter;
+
+        public Feuerwehreinsatz(string ort, int alarmstufe)
+        {
+            this.ort = ort;
+            this.alarmstufe = alarmstufe;
+        }
+
+        public void UpdateAlarmstufe(int alarmstufe)
+        {
+            this.alarmstufe = alarmstufe;
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Feuerwehreinsatz einsatz = new Feuerwehreinsatz("Wien", 2);
+
+            Console.WriteLine("Einsatzort: " + einsatz.ort);
+            Console.WriteLine("Alarmstufe: " + einsatz.alarmstufe);
+
+            einsatz.UpdateAlarmstufe(3);
+
+            Console.WriteLine("Alarmstufe: " + einsatz.alarmstufe);
+        }
+    }
+}
